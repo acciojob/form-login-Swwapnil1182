c7f85c6d372ebb9e4cbe3dbdd4845c94e2559233
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-15 18:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
 
+	const fname=document.getElementById("first");
+	const lname=document.getElementById("last");
+
+	const msg=`${fname} ${lname}`
+
 }
